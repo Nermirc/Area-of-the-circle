9c8dac07bd2c72e17da382c06c5c90228d925498
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-10-24 21:42:16 +0300

    Area of circle
    calculating area of circle by using DEFİNİTİON PI

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.141
+int main()
+{
+    float yaricap,alan;
+    printf("Lutfen yaricapi giriniz.\n");
+    scanf("%f",&yaricap);
+    alan=PI*yaricap*yaricap;
+    printf("Dairenin alani : %f",alan);
+    return 0 ;
+}
